1ea82da04b82df5a602ab19124aec61b79b24739
Ciprian <cyprian.bodnar@gmail.com>
2019-05-16 23:36:19 +0300

    restructuring the project tree files

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def wizzard():
+    return jsonify("Hocus Pocus")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5050, debug=True)
